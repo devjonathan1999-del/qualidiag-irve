50bff0d265d669f9747c6d48ae86f49dc40da7c6
devjonathan1999-del <dev.jonathan1999@gmail.com>
2026-09-08 09:05:50 +0200

    feat: appliquer la politique véhicule au tronc commun

--- app/data.js
+++ app/data.js
@@ -14,6 +14,7 @@ const DATA_FILES = {
   nodes: NODE_FILES,
   powerPolicy: 'power-policy.json',
   autocontrolPolicy: 'autocontrol-policy.json',
+  vehiclePolicy: 'vehicle-policy.json',
   schneiderChargePolicy: 'schneider-charge-policy.json',
   schneiderWiserPolicy: 'schneider-wiser-policy.json',
   schneiderSmartchargePolicy: 'schneider-smartcharge-policy.json',
@@ -153,6 +154,7 @@ export async function loadData(baseUrl = '../data/') {
   const data = Object.fromEntries(entries);
   data.nodes = applyPowerPolicy(data.nodes, data.powerPolicy);
   data.nodes = applySchneiderChargePolicy(data.nodes, data.autocontrolPolicy);
+  data.nodes = applySchneiderChargePolicy(data.nodes, data.vehiclePolicy);
   data.nodes = applySchneiderChargePolicy(data.nodes, data.schneiderChargePolicy);
   data.nodes = applySchneiderChargePolicy(data.nodes, data.schneiderWiserPolicy);
   data.nodes = applySchneiderChargePolicy(data.nodes, data.schneiderSmartchargePolicy);
